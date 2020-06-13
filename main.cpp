b2a5df378b2773c5ad7390dd0d41e23ba3c66eb1
Michał Sarzyński <ms371678@students.mimuw.edu.pl>
2020-06-13 23:10:45 +0200

    Add printing results to main.cpp

--- main.cpp
+++ main.cpp
@@ -8,15 +8,16 @@
 #include "verlet-integration.h"
 
 int main(int argc, char *argv[]) {
-    int num_processes, rank;
-    double dt = 1;
-    int steps = 5;
-
-    const char* filename = "./test2.in";
+    int num_processes, rank, steps;
+    double dt;
+    bool verbose;
+    std::string input, output;
 
     MPI_Init(&argc, &argv);
     MPI_Comm_rank(MPI_COMM_WORLD, &rank);
     MPI_Comm_size(MPI_COMM_WORLD, &num_processes);
+
+    parseArgs(argc, argv, dt, steps, input, output, verbose);
 
     std::cout << std::setprecision(16);
 
@@ -24,7 +25,7 @@ int main(int argc, char *argv[]) {
     int particles_sz, my_particles_sz;
 
     if (rank == 0) {
-        particles_sz = parseParticles(filename, &particles);
+        particles_sz = parseParticles(input, &particles);
     }
 
     MPI_Bcast(&particles_sz, 1, MPI_INT, 0, MPI_COMM_WORLD);
@@ -32,7 +33,8 @@ int main(int argc, char *argv[]) {
     int* send_counts = generateBuffSizes(num_processes, particles_sz);
     int* displacement = generateDisplacement(send_counts, particles_sz);
 
-    my_particles = scatterParticles(&particles, particles_sz, num_processes, rank, send_counts, displacement);
+    my_particles = scatterParticles(&particles, particles_sz, num_processes,
+                                    rank, send_counts, displacement);
     my_particles_sz = bufferSize(rank, num_processes, particles_sz);
 
     for (int i = 0; i < steps; i++) {
@@ -44,12 +46,19 @@ int main(int argc, char *argv[]) {
         calcNewCoor(my_particles, my_particles_sz, dt);
         embeddedAlgorithm(my_particles, rank, num_processes, particles_sz);
         calcNewAccAndV(my_particles, my_particles_sz, dt);
+
+        if (verbose) {
+            gatherParticles(particles, my_particles, rank,
+                            send_counts, displacement);
+            if (rank == 0) {
+                saveResults(particles, particles_sz, output, i + 1);
+            }
+        }
     }
 
     gatherParticles(particles, my_particles, rank, send_counts, displacement);
-
     if (rank == 0) {
-        printParticles(particles, particles_sz);
+        saveResults(particles, particles_sz, output, steps);
     }
 
     MPI_Finalize();
